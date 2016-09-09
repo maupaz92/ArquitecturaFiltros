259117efe83e8eff155d09999ede31efe98d77fb
Mauricio Paz <maupaz92@hotmail.com>
2016-09-08 21:37:32 -0600

    trabajo sobre el buffer que guarda datos de mem

--- prueba3/source_code/buffer_pixeles_mem.v
+++ prueba3/source_code/buffer_pixeles_mem.v
@@ -22,24 +22,30 @@ module buffer_pixeles_mem #(
 	input read_pixel,
 	input save_mem_data,
 	// 
-	output reg [PIXEL_BITS-1:0] pixel
+	output reg [PIXEL_BITS-1:0] pixel,
+	output buffer_full,
+	output data_available 
 );
 
 
 //=========================================================================
-	localparam BITS_SELECC_REGS = 3;
+	localparam BITS_SELECC_REGS = 4;
 	
 	localparam 
 		E_INICIO		= 0,
 		E_GRUPO_1	= 1,
-		E_GRUPO_2	= 2,
-		E_ESPERA_1	= 3,
-		E_ESPERA_2	= 4;
+		E_ESPERA_1	= 2,
+		E_GRUPO_2	= 3,
+		E_ESPERA_2	= 4,
+		E_GRUPO_3	= 5,
+		E_ESPERA_3	= 6,
+		E_GRUPO_4	= 7,
+		E_ESPERA_4	= 8;
 
 //=========================================================================
 
 
-	reg [2:0] e_actual, e_siguiente;
+	reg [3:0] e_actual, e_siguiente;
 	
 	
 	// senales para dirigir los valores de los registros
@@ -51,21 +57,40 @@ module buffer_pixeles_mem #(
 	wire [PIXEL_BITS-1:0] reg_pix_6_value;
 	wire [PIXEL_BITS-1:0] reg_pix_7_value;
 	wire [PIXEL_BITS-1:0] reg_pix_8_value;
+	wire [PIXEL_BITS-1:0] reg_pix_9_value;
+	wire [PIXEL_BITS-1:0] reg_pix_10_value;
+	wire [PIXEL_BITS-1:0] reg_pix_11_value;
+	wire [PIXEL_BITS-1:0] reg_pix_12_value;
+	wire [PIXEL_BITS-1:0] reg_pix_13_value;
+	wire [PIXEL_BITS-1:0] reg_pix_14_value;
+	wire [PIXEL_BITS-1:0] reg_pix_15_value;
+	wire [PIXEL_BITS-1:0] reg_pix_16_value;
 	
-	// senal para seleccionar cual registro sera la salida del mux
-	wire [BITS_SELECC_REGS-1:0] seleccion_regs;
 	
 	// senales para habilitar el grupo de registros
 	wire habilitar_primer_grupo;
 	wire habilitar_segundo_grupo;
+	wire habilitar_tercer_grupo;
+	wire habilitar_cuarto_grupo;
 	
 	
-	wire grupo_uno_leido;
-	wire grupo_dos_leido;
-	wire buffer_lleno;
+	wire activar_grupo1;
+	wire activar_grupo2;
+	wire activar_grupo3;
+	wire activar_grupo4;
+	
+	wire desactivar_grupo1;
+	wire desactivar_grupo2;
+	wire desactivar_grupo3;
+	wire desactivar_grupo4;
+	
+	wire estado_grupo1;
+	wire estado_grupo2;
+	wire estado_grupo3;
+	wire estado_grupo4;
 	
 	
-		
+	
 //==========================================================================	
 // estado del registro
 	always @(posedge clk) begin
@@ -93,6 +118,22 @@ module buffer_pixeles_mem #(
 					e_siguiente = E_GRUPO_2;
 			end
 			E_GRUPO_2: begin
+				if(save_mem_data)
+					e_siguiente = E_ESPERA_1;
+			end
+			E_ESPERA_3: begin
+				if(grupo_uno_leido)
+					e_siguiente = E_GRUPO_1;
+			end
+			E_GRUPO_3: begin
+				if(save_mem_data)
+					e_siguiente = E_ESPERA_1;
+			end
+			E_ESPERA_4: begin
+				if(grupo_uno_leido)
+					e_siguiente = E_GRUPO_1;
+			end
+			E_GRUPO_4: begin
 				if(save_mem_data)
 					e_siguiente = E_ESPERA_1;
 			end
@@ -129,6 +170,22 @@ module buffer_pixeles_mem #(
 				pixel = reg_pix_7_value;
 			7:
 				pixel = reg_pix_8_value;
+			8:
+				pixel = reg_pix_1_value;
+			9:
+				pixel = reg_pix_2_value;
+			10:
+				pixel = reg_pix_3_value;
+			11:
+				pixel = reg_pix_4_value;
+			12:
+				pixel = reg_pix_5_value;
+			13:
+				pixel = reg_pix_6_value;
+			14:
+				pixel = reg_pix_7_value;
+			15:
+				pixel = reg_pix_8_value;
 			default:
 				pixel = reg_pix_1_value;
 		endcase
@@ -136,41 +193,64 @@ module buffer_pixeles_mem #(
 
 
 
+//=========================================================================	
+	registro_sumador ff_estado_grupo1 (
+	 .clk(clk), 
+	 .reset(reset),	 
+	 .sumar(activar_grupo1 || desactivar_grupo1),
+	 .resultado(estado_grupo1)
+	);
+	
+	defparam ff_estado_grupo1.CANTIDAD_SUMA = 1;
+	defparam ff_estado_grupo1.BITS_DATOS = 1;	
+
+	
+//=========================================================================	
+	registro_sumador ff_estado_grupo2 (
+	 .clk(clk), 
+	 .reset(reset),	 
+	 .sumar(activar_grupo2 || desactivar_grupo2),
+	 .resultado(estado_grupo2)
+	);
+	
+	defparam ff_estado_grupo2.CANTIDAD_SUMA = 1;
+	defparam ff_estado_grupo2.BITS_DATOS = 1;
+
+	
+//=========================================================================	
+	registro_sumador ff_estado_grupo3 (
+	 .clk(clk), 
+	 .reset(reset),	 
+	 .sumar(activar_grupo3 || desactivar_grupo3),
+	 .resultado(estado_grupo3)
+	);
+	
+	defparam ff_estado_grupo3.CANTIDAD_SUMA = 1;
+	defparam ff_estado_grupo3.BITS_DATOS = 1;
+
+	
+//=========================================================================	
+	registro_sumador ff_estado_grupo4 (
+	 .clk(clk), 
+	 .reset(reset),	 
+	 .sumar(activar_grupo4 || desactivar_grupo4),
+	 .resultado(estado_grupo4)
+	);
+	
+	defparam ff_estado_grupo4.CANTIDAD_SUMA = 1;
+	defparam ff_estado_grupo4.BITS_DATOS = 1;
+
 
 //=========================================================================
 
-	assign habilitar_primer_grupo = (e_actual == E_GRUPO_1) || (e_actual == E_INICIO);
-	assign habilitar_segundo_grupo = (e_actual == E_GRUPO_2);	
-	
-	assign grupo_uno_leido = (seleccion_regs >= 4) ? 1 : 0;
-	assign grupo_dos_leido = ~grupo_uno_leido;
-	
-	assign buffer_lleno = (e_actual == E_ESPERA_1) || (e_actual == E_ESPERA_2);
-	
-	
-/*
- *
-==========================================================================
-CONTEXTO: modulo que indica cual es el valor de los registros que se va a presentar
-en la salida. Cada vez que se aumente el valor, se va a habilitar otro registro
-para que sea la salida
-*
-*/
-	registro_sumador habilitador_registros (
-	 .clk(clk), 
-	 .reset(reset),	 
-	 .sumar(read_pixel),
-	 .resultado(seleccion_regs)
-	);
-	
-	defparam habilitador_registros.CANTIDAD_SUMA = 1;
-	defparam habilitador_registros.BITS_DATOS = BITS_SELECC_REGS;	
-	
-	
+		assign activar_grupo1 = (e_siguiente == E_GRUPO_2);
 
+	
+	
+	
+//=========================================================================
+	
 // ++++++++++++++++++++++++++++ de aqui para abajo solo registros 		
-	
-
 
 	FlipFlopD_Habilitado reg_pix_1 (
 		 .clk(clk), 
@@ -273,6 +353,110 @@ para que sea la salida
 		
 	defparam reg_pix_8.BITS_EN_REGISTRO = PIXEL_BITS;
 
+	
+	
+//=========================================================================
+	
+	FlipFlopD_Habilitado reg_pix_9 (
+		 .clk(clk), 
+		 .reset(reset), 
+		 .habilitador(habilitar_primer_grupo && save_mem_data), 
+		 .datos_entrada(memory_data[31:24]),
+		 .datos_salida(reg_pix_9_value)
+		);
+		
+	defparam reg_pix_9.BITS_EN_REGISTRO = PIXEL_BITS;
+
+
+//=========================================================================
+
+	FlipFlopD_Habilitado reg_pix_10 (
+		 .clk(clk), 
+		 .reset(reset), 
+		 .habilitador(habilitar_tercer_grupo && save_mem_data), 
+		 .datos_entrada(memory_data[23:16]),
+		 .datos_salida(reg_pix_10_value)
+		);
+		
+	defparam reg_pix_10.BITS_EN_REGISTRO = PIXEL_BITS;
+
+	
+//=========================================================================
+
+	FlipFlopD_Habilitado reg_pix_11 (
+		 .clk(clk), 
+		 .reset(reset), 
+		 .habilitador(habilitar_tercer_grupo && save_mem_data), 
+		 .datos_entrada(memory_data[15:8]),
+		 .datos_salida(reg_pix_11_value)
+		);
+		
+	defparam reg_pix_11.BITS_EN_REGISTRO = PIXEL_BITS;
+
+	
+//=========================================================================
+
+	FlipFlopD_Habilitado reg_pix_12 (
+		 .clk(clk), 
+		 .reset(reset), 
+		 .habilitador(habilitar_tercer_grupo && save_mem_data), 
+		 .datos_entrada(memory_data[7:0]),
+		 .datos_salida(reg_pix_12_value)
+		);
+		
+	defparam reg_pix_12.BITS_EN_REGISTRO = PIXEL_BITS;
+
+	
+//=========================================================================
+
+	FlipFlopD_Habilitado reg_pix_13 (
+		 .clk(clk), 
+		 .reset(reset), 
+		 .habilitador(habilitar_segundo_grupo && save_mem_data), 
+		 .datos_entrada(memory_data[31:24]),
+		 .datos_salida(reg_pix_13_value)
+		);
+		
+	defparam reg_pix_13.BITS_EN_REGISTRO = PIXEL_BITS;
+
+	
+//=========================================================================
+
+	FlipFlopD_Habilitado reg_pix_14 (
+		 .clk(clk), 
+		 .reset(reset), 
+		 .habilitador(habilitar_cuarto_grupo && save_mem_data), 
+		 .datos_entrada(memory_data[23:16]),
+		 .datos_salida(reg_pix_14_value)
+		);
+		
+	defparam reg_pix_14.BITS_EN_REGISTRO = PIXEL_BITS;
+
+	
+//=========================================================================
+
+	FlipFlopD_Habilitado reg_pix_15 (
+		 .clk(clk), 
+		 .reset(reset), 
+		 .habilitador(habilitar_cuarto_grupo && save_mem_data), 
+		 .datos_entrada(memory_data[15:8]),
+		 .datos_salida(reg_pix_15_value)
+		);
+		
+	defparam reg_pix_15.BITS_EN_REGISTRO = PIXEL_BITS;
+
+	
+//=========================================================================
+
+	FlipFlopD_Habilitado reg_pix_16 (
+		 .clk(clk), 
+		 .reset(reset), 
+		 .habilitador(habilitar_cuarto_grupo && save_mem_data), 
+		 .datos_entrada(memory_data[7:0]),
+		 .datos_salida(reg_pix_16_value)
+		);
+		
+	defparam reg_pix_16.BITS_EN_REGISTRO = PIXEL_BITS;
 
 
 
